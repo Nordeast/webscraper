2e6c97708029efd2622b19d6abde3366886e4022
Allen Rand <arand@design-center.us>
2019-04-25 13:31:25 -0500

    Added progress bar and writing out to a file to the sql codes script

--- webscraper_sqlcodes.py
+++ webscraper_sqlcodes.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import soup_engine
 import profile
 import constants
+import progress_bar
 
 # Code taken from the tutorial here https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
 # and here https://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php
@@ -22,12 +23,16 @@ sql_code_dictionaries = []
 
 #### START SCRAPE ####
 
+print('Scraping: ' + page_url)
+
 # Get the main page of the reason codes parsed into a BeautifulSoup instance
 main_page_soup = soup_engine.soup_from_url(page_url)
-
 # Find all links to reason code groups
 main_page_link_spans = main_page_soup.find_all(
     'span', class_="ulchildlinktext")
+
+index = 1
+total_length = len(main_page_link_spans)
 
 for span in main_page_link_spans:
     a_tag = span.find("a")
@@ -39,10 +44,18 @@ for span in main_page_link_spans:
     # Parse code page
     parsed_tags = soup_engine.dict_from_soup(soup)
     parsed_tags[constants.DICT_URL] = ': ' + url
-    # Print results to console
-    soup_engine.print_soup_engine_dict(parsed_tags)
     # Append to array
     sql_code_dictionaries.append(parsed_tags)
+
+    progress_bar.print(index, total_length, 'Progress',
+                       '(' + str(index) + '/' + str(total_length) + ') Pages Scraped')
+    index += 1
+
+
+# Write to file
+file_name = 'sql_codes'
+print('Writing parsed codes to file with name ' + file_name)
+soup_engine.write_soup_engine_dict_to_file(sql_code_dictionaries, file_name)
 
 #### END SCRAPE ####
 
