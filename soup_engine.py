79d155dc6f725f9a20eb7f7194a6f58b39c0c8b4
Allen Rand <allenrandb@gmail.com>
2019-04-30 23:56:55 -0500

    Converted the soup_engine to output a structured dictionary and converted the file write to write json

--- soup_engine.py
+++ soup_engine.py
@@ -1,12 +1,12 @@
 import time
-from datetime import datetime
+import calendar
 import requests
 import bs4
 from bs4 import BeautifulSoup
 import constants
 import collections
 import os
-
+import json
 
 def soup_from_url(url):
     """This method takes a browser instance and a url. It then navigates to the page, pulls the html from it
@@ -34,32 +34,72 @@ def dict_from_soup(soup):
     for dictionary in constants.PARSE_DICT:
         tag = soup.find(dictionary[constants.DICT_TAG],
                         class_=dictionary[constants.DICT_CLASS])
+
         if tag:
-            parsed_dict[dictionary[constants.DICT_KEY]
-                        ] = parse_tag(tag, '', [])
+            if dictionary[constants.DICT_CLASS] == 'msgId' or dictionary[constants.DICT_CLASS] == 'msgText':
+                parsed_dict[dictionary[constants.DICT_KEY]] = [
+                    {
+                        'type': convert_tag_name(tag.name),
+                        'content': clean_string(str(tag.get_text())),
+                        'children': []
+                    }
+                ]
+            else:    
+                parsed_dict[dictionary[constants.DICT_KEY]] = parse_tag(tag, None, [])
         else:
             parsed_dict[dictionary[constants.DICT_KEY]] = []
 
     return parsed_dict
 
 
-def parse_tag(tag, spacing='', parsed_tags=[]):
+def parse_tag(tag, parent=None, parsed_tags=[]):
     """ 
     Recurses down a tags tree and parses out the information.
-    It represents the tree by element > child element : content
+    It represents the tree in a dictionary
     """
+    text_tags = ['p', 'span', 'var', 'h1', 'h2', 'h3', 'h4', 'h5']
     for content in tag.contents:
-        if type(content) is bs4.element.Tag:
-            carrot = ''
-            if spacing:
-                carrot = ' > '
+        # Check if it is a html tag
+        is_tag = type(content) is bs4.element.Tag
+        # Check if it is a string
+        is_string = type(content) is bs4.element.NavigableString and str_is_not_empty(str(content))
+
+        # Check if it a html tag that is a wrapper for a string
+        is_text_tag = False
+        if is_tag:
+            is_text_tag = str(content.name) in text_tags
+
+        if is_string or is_text_tag:
+            # The element is just a string with no children so just get the text
+            string = ''
+            if is_tag:
+                string = clean_string(str(content.get_text()))
+            else:
+                string = clean_string(str(content))
+
+            # Skip if the string is empty
+            if str_is_empty(string):
+                continue
+
+            if parent:
+                # We have a parent so just add the text to the parent element
+                parent['content'] = parent['content'] + string
+            else:
+                parsed_tags.append({
+                    'type': 'string',
+                    'content': string
+                })
+        elif is_tag:            
             # If the content is a tag we need to recurse down
-            # and find its content
-            parse_tag(content, spacing + carrot + content.name, parsed_tags)
-        elif type(content) is bs4.element.NavigableString and not str(content).isspace():
-            # Content was a string so we need to grab its content
-            parsed_tag = spacing + ': ' + clean_string(str(content))
-            parsed_tags.append(parsed_tag)
+            # and continue parsing its children
+            dictionary = {
+                'type': convert_tag_name(content.name),
+                'content': '',
+                'children': []
+            }
+            parsed_tags.append(dictionary)
+            parse_tag(content, dictionary, dictionary['children'])
+
     return parsed_tags
 
 
@@ -70,29 +110,30 @@ def clean_string(string):
 
     string = string.replace('\n', ' ')
     string = string.replace('  ', ' ')
-    string = string.strip()
+    string = string.encode('ascii', 'ignore').decode()
     return string
 
+def convert_tag_name(string):
+    if string == 'div':
+        return 'section'
+    elif string == 'ul' or string == 'dl':
+        return 'list'
+    elif string == 'li':
+        return 'bullet'
+    elif string == 'dt':
+        return 'description title'
+    elif string == 'dd':
+        return 'description'
+    elif string == 'p' or string == 'span' or string == 'var':
+        return 'string'
+    elif string == 'h4':
+        return 'header'
 
-def print_soup_engine_dict(dictionary):
-    """ 
-    This method is for debugging purposes. It takes a dict_from_soup dictionary
-    and pretty prints it to the console.
-    """
-
-    print('<<<<<<<<<<>>>>>>>>>>')
-    print('')
-    for parse_dict in constants.PARSE_DICT:
-        value = dictionary[parse_dict[constants.DICT_KEY]]
-        print(parse_dict[constants.DICT_KEY])
-        for string in value:
-            print('    ' + string)
-
-    print(constants.DICT_URL)
-    print('    ' + dictionary[constants.DICT_URL])
-    print('')
-    print('<<<<<<<<<<>>>>>>>>>>')
-
+def str_is_empty(string):
+    return not string or string.isspace()
+    
+def str_is_not_empty(string):
+    return not str_is_empty(string)
 
 def write_soup_engine_dict_to_file(dictionaries, file_name):
     """
@@ -100,20 +141,9 @@ def write_soup_engine_dict_to_file(dictionaries, file_name):
     and timestamp.
     """
     # Create file name and path
-    file_name = file_name + '_' + str(time.clock()) + ".txt"
+    file_name = file_name + '_' + str(calendar.timegm(time.gmtime())) + ".json"
     folder_path = os.path.join(os.getcwd(), 'Output_Files')
     file_path = os.path.join(folder_path, file_name)
     # Open file for writing
-    with open(file_path, 'a') as txt_file:
-        # Begin looping and writing to the file
-        for dictionary in dictionaries:
-            txt_file.write('**start section**\n')
-            for parse_dict in constants.PARSE_DICT:
-                value = dictionary[parse_dict[constants.DICT_KEY]]
-                txt_file.write(parse_dict[constants.DICT_KEY] + '\n')
-                for string in value:
-                    txt_file.write('    ' + string + '\n')
-
-            txt_file.write(constants.DICT_URL + '\n')
-            txt_file.write('    ' + dictionary[constants.DICT_URL] + '\n')
-            txt_file.write('**end section**\n')
+    with open(file_path, 'a') as json_file:
+        json.dump(dictionaries, json_file, skipkeys=False, ensure_ascii=True, check_circular=True, allow_nan=True, cls=None, indent=4)
