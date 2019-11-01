6041830af00a202d0b947dada4d174fb74692983
Allen Rand <allenrandb@gmail.com>
2019-10-31 19:09:40 -0500

    Update to parse all IE system messages

--- system_messages.py
+++ system_messages.py
@@ -9,8 +9,30 @@ import progress_bar
 # Code taken from the tutorial here https://medium.freecodecamp.org/how-to-scrape-websites-with-python-and-beautifulsoup-5946935d93fe
 # and here https://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php
 
-page_url = 'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEF_messages.htm?view=embed'
+urls = [
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEF_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEFA_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iefa.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEFC_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEFE_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEFI_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEFJ_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEH_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEW0000_-_0999.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IEW1001_-_1999.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/pmbmes.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/pmbmes1.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/pmbmes2.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iew5.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IFA_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IFB_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IFC_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IGD_messages.htm?view=embed',
+    'https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/iea3m8_IGDH_messages.htm?view=embed'
+]
+
 url_base = "https://www.ibm.com/support/knowledgecenter/en/SSLTBW_2.1.0/com.ibm.zos.v2r1.ieam800/"
+
 url_view_embed = "?view=embed"
 PARSE_DICT = constants.SYSTEM_MESSAGES_PARSE_DICT
 
@@ -23,35 +45,35 @@ profile.start()
 parsed_dictionaries = []
 
 #### START SCRAPE ####
+for link in urls:
+    print('Scraping: ' + link)
 
-print('Scraping: ' + page_url)
+    # Get the main page of the reason codes parsed into a BeautifulSoup instance
+    main_page_soup = soup_engine.soup_from_url(link)
+    # Find all links to reason code groups
+    main_page_link_spans = main_page_soup.find_all(
+        'span', class_="ulchildlinktext")
 
-# Get the main page of the reason codes parsed into a BeautifulSoup instance
-main_page_soup = soup_engine.soup_from_url(page_url)
-# Find all links to reason code groups
-main_page_link_spans = main_page_soup.find_all(
-    'span', class_="ulchildlinktext")
+    index = 1
+    total_length = len(main_page_link_spans)
 
-index = 1
-total_length = len(main_page_link_spans)
+    for span in main_page_link_spans:
+        a_tag = span.find("a")
+        parsed_url = a_tag['href'].replace("../../", "")
+        parsed_url = parsed_url.split('#', 1)[0]
 
-for span in main_page_link_spans:
-    a_tag = span.find("a")
-    parsed_url = a_tag['href'].replace("../../", "")
-    parsed_url = parsed_url.split('#', 1)[0]
+        # Load group page and get soup
+        url = url_base + parsed_url
+        soup = soup_engine.soup_from_url(url + url_view_embed)
+        # Parse code page
+        parsed_tags = soup_engine.dict_from_soup(soup, PARSE_DICT)
+        parsed_tags[constants.DICT_URL] = url
+        # Append to array
+        parsed_dictionaries.append(parsed_tags)
 
-    # Load group page and get soup
-    url = url_base + parsed_url
-    soup = soup_engine.soup_from_url(url + url_view_embed)
-    # Parse code page
-    parsed_tags = soup_engine.dict_from_soup(soup, PARSE_DICT)
-    parsed_tags[constants.DICT_URL] = url
-    # Append to array
-    parsed_dictionaries.append(parsed_tags)
-
-    progress_bar.print(index, total_length, 'Progress',
-                       '(' + str(index) + '/' + str(total_length) + ') Pages Scraped')
-    index += 1
+        progress_bar.print(index, total_length, 'Progress',
+                           '(' + str(index) + '/' + str(total_length) + ') Pages Scraped')
+        index += 1
 
 
 # Write to file
